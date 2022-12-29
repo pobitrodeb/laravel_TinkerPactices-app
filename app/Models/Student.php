6d27672b7739b0454f216bb346e2a3ffd8ba4b31
Pobitrodeb <pobitrodn@gmail.com>
2022-12-29 23:36:12 +0600

    successfully done laravel tinker pactices insert, create, update, delete

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+    protected $fillable =[
+        "roll",
+        "name",
+        "email",
+        "created_at",
+        "updated_at",
+    ];
 }
--- database/migrations/2022_12_29_164306_create_students_table.php
+++ database/migrations/2022_12_29_164306_create_students_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->integer('roll');
+            $table->string('name');
+            $table->string('email');
             $table->timestamps();
         });
     }
